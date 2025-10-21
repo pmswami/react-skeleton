f20267febe1e23a65d63268c5f0c16d535711b77
Swam Fire <usern5949@gmail.com>
2025-10-21 16:13:54 +0530

    Added data fetching logic

--- react-skeleton-screens/src/components/Articles.js
+++ react-skeleton-screens/src/components/Articles.js
@@ -1,7 +1,35 @@
+import { useState, useEffect } from "react";
+
 const Articles = () => {
+
+    const [articles, setArticles] = useState(null)
+
+    useEffect(() => {
+        const timer = setTimeout(async () => {
+            try {
+                const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+                const data = await res.json();
+                setArticles(data);
+            } catch (err) {
+                console.error("Failed to fetch posts:", err);
+            }
+            }, 5000);
+    
+            return () => clearTimeout(timer);
+        }, []);
+
     return ( 
         <div className="articles">
             <h2>Articles</h2>
+            {articles && articles.map(article =>
+                <div className="article" key={article.id}>
+                    <h3>{article.title}</h3>
+                    <p>{article.body}</p>
+                </div>
+            )}
+            {!articles && (
+                <div>Loading ...</div>
+            )}
         </div>
      );
 }
--- react-skeleton-screens/src/components/User.js
+++ react-skeleton-screens/src/components/User.js
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react";
+
 const User = () => {
-    return ( 
+
+    const [profile, setProfile] = useState(null)
+
+    // useEffect(()=>{
+    //     setTimeout( async()=>{
+    //         const res = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    //         const data = res.json()
+    //         setProfile(data)
+    //     }, 5000)
+    // })
+
+    useEffect(() => {
+    const timer = setTimeout(async () => {
+        try {
+            const res = await fetch("https://jsonplaceholder.typicode.com/users/1");
+            const data = await res.json();
+            setProfile(data);
+        } catch (err) {
+            console.error("Failed to fetch profile:", err);
+        }
+        }, 5000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
         <div className="user">
             <h2>User Details</h2>
+            {profile && (
+                <div className="profile">
+                    <h3>{profile.username}</h3>
+                    <p>{profile.email}</p>
+                    <a href={profile.website}>{profile.website}</a>
+                </div>
+            )}
+
+            {!profile && (
+                <div>Loading ...</div>
+            )}
         </div>
      );
 }
